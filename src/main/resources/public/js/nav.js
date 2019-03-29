53c59c5b566652dae16335bb617c3d0ddd726739
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-29 14:48:55 +0000

    nav

--- src/main/resources/public/js/nav.js
+++ src/main/resources/public/js/nav.js
@@ -1,14 +1,18 @@
 // Setting up the Variables
 var bars = document.getElementById("nav-action");
 var nav = document.getElementById("nav");
+var navShowing = false;
 
-
-//setting up the listener
-bars.addEventListener("click", barClicked, false);
-
+$(document).click(function (event) {
+    var target = $(event.target);
+    if (!(target.closest('#nav-list').length) && navShowing || target.closest('#nav-action').length) {
+        barClicked();
+    }
+});
 
 //setting up the clicked Effect
 function barClicked() {
   bars.classList.toggle('active');
   nav.classList.toggle('visible');
+  navShowing = !navShowing;
 }
