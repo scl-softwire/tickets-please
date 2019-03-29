db78bbbf2ec918035ca65c9df970f2d21b8abc4b
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-29 11:13:30 +0000

    Create Ticket.java

---
+++ src/main/java/com/softwire/training/tickets/model/Ticket.java
@@ -0,0 +1,41 @@
+package com.softwire.training.tickets.model;
+
+public class Ticket {
+
+    private int id;
+    private String name;
+    private String description;
+    private boolean resolved;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        this.resolved = resolved;
+    }
+}
