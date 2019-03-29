0b673ffbf38d480bf6076f834bbad69cfbabee51
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-03-29 10:37:57 +0000

    Remove connection pooling

--- src/main/java/com/softwire/training/tickets/db/DbConfig.java
+++ src/main/java/com/softwire/training/tickets/db/DbConfig.java
@@ -1,14 +1,10 @@
 package com.softwire.training.tickets.db;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
@@ -21,18 +17,7 @@ public class DbConfig {
     }
 
     @Bean
-    public DataSource dataSource() {
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            return new HikariDataSource();
-        } else {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
-    }
-
-    @Bean
     public Jdbi jdbi() {
-        return Jdbi.create(dataSource());
+        return Jdbi.create(dbUrl);
     }
 }
