35137854fd0a38ae4661e96503ff7bf43d94f946
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-03-29 09:51:01 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/tickets-please-1.0.jar
---
+++ src/main/java/com/softwire/training/tickets/services/SampleService.java
@@ -0,0 +1,29 @@
+package com.softwire.training.tickets.services;
+
+import org.jdbi.v3.core.Jdbi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+public class SampleService {
+
+    private final Jdbi jdbi;
+
+    @Autowired
+    public SampleService(Jdbi jdbi) {
+        this.jdbi = jdbi;
+    }
+
+    public List<Timestamp> sample() {
+        return jdbi.withHandle(handle -> {
+            handle.execute("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            handle.execute("INSERT INTO ticks VALUES (now())");
+            return handle.createQuery("SELECT tick FROM ticks")
+                    .mapTo(Timestamp.class)
+                    .list();
+        });
+    }
+}
